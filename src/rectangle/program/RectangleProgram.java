0a787b808d4662db014d2f96ad4f2592c2faa316
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-03-19 18:02:13 +0700

    Complete

---
+++ src/rectangle/program/RectangleProgram.java
@@ -0,0 +1,18 @@
+package rectangle.program;
+
+import java.util.Scanner;
+
+public class RectangleProgram {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Width :");
+        width = sc.nextFloat();
+        System.out.println("Enter Height :");
+        height = sc.nextFloat();
+        float arena = width * height;
+        System.out.println("Arena is :" + arena);
+    }
+}
---
+++ src/time/system/TimeSystem.java
@@ -0,0 +1,10 @@
+package time.system;
+
+import java.util.Date;
+
+public class TimeSystem {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
