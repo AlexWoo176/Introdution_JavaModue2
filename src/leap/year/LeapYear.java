19ce7e5c627548353975c58cb6af3e770f212736
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-03-21 19:58:07 +0700

    Complete

---
+++ src/leap/year/LeapYear.java
@@ -0,0 +1,27 @@
+package leap.year;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.println("Nhập vào năm kiểm tra: ");
+        year = sc.nextInt();
+        boolean isLeapYear = false;
+        if(year % 4 == 0) {
+            if( year % 100 == 0) {
+                if(year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if(isLeapYear) {
+            System.out.println(year + " là năm nhuận");
+        } else {
+            System.out.println(year + " không là năm nhuận");
+        }
+    }
+}
